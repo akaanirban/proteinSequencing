128bcd3a8e9db0e17944ba94355d0c1cf19efa2e
akaanirban <anirban.dash@gmail.com>
2017-07-18 13:36:40 -0400

    Added functions

--- possible_combinations.py
+++ possible_combinations.py
@@ -14,21 +14,41 @@ import csv
 # print(all_combinations)
 #==============================================================================
 
+def writeToTextFile(combinations, filename):  #if you want text file
+    filename = filename + '.txt'
+    with open(filename, 'w') as myfile:
+        myfile.write(" ".join(combinations))
+    print("Generated text file :", filename)
 
-proteinSeq = 'MTEYKLVVVGAGGVGKSALTIQLIQNHFVDEYDPTIEDSYRKQVVIDGETCLLDILDTAGQEEYSAMRDQYMRTGEGFLCVFAINNTKSFEDIHHYREQIKRVKDSEDVPMVLVGNKCDLPSRTVDTKQAQDLARSYGIPFIETSAKTRQRVEDAFYTLVREIRQYRLKKISKEEKTPGCVKIKKCIIM'
-#proteinSeq = 'MTEYK'
-validAlphabet = 'GALMFWKQESPVICYHRNDT'
-allCombinations = list()
-for i in range(len(proteinSeq)):
-    for letter in validAlphabet:
-        dummySeq = list(proteinSeq)
-        dummySeq[i] = letter
-        dummySeq = ''.join(dummySeq)
-        allCombinations.append(dummySeq)
-totalGenerated = len(allCombinations)
-print("Number of entries generated = ", totalGenerated)
-filename = 'proteinSequence'+ str(totalGenerated) + '.csv'
-with open(filename, 'w') as myfile:
-    wr = csv.writer(myfile,delimiter=' ', quoting=csv.QUOTE_NONE)
-    wr.writerow(allCombinations)
+#==============================================================================
+def writeToCSV(combinations, filename): #if you want csv
+    filename = filename + '.csv'
+    delimeter = ',' #enter the delimeter you want
+    with open(filename, 'w') as myfile:
+        wr = csv.writer(myfile,delimiter= delimeter, quoting=csv.QUOTE_NONE) 
+        wr.writerow(combinations)
+    print("Generated CSV file :", filename)
+
+
+
+if __name__ == '__main__':
+    proteinSeq = 'MTEYKLVVVGAGGVGKSALTIQLIQNHFVDEYDPTIEDSYRKQVVIDGETCLLDILDTAGQEEYSAMRDQYMRTGEGFLCVFAINNTKSFEDIHHYREQIKRVKDSEDVPMVLVGNKCDLPSRTVDTKQAQDLARSYGIPFIETSAKTRQRVEDAFYTLVREIRQYRLKKISKEEKTPGCVKIKKCIIM'
+    #proteinSeq = 'MTEYK'
+    validAlphabet = 'GALMFWKQESPVICYHRNDT'
+    allCombinations = list()
+    for i in range(len(proteinSeq)):
+        for letter in validAlphabet:
+            dummySeq = list(proteinSeq)
+            dummySeq[i] = letter
+            dummySeq = ''.join(dummySeq)
+            allCombinations.append(dummySeq)
+    totalGenerated = len(allCombinations)
+    print("Number of entries generated = ", totalGenerated)
+    name_of_generated_File = 'proteinSequence_'+ str(totalGenerated)
+    #generate text file
+    writeToTextFile(allCombinations, name_of_generated_File)
+    #generate CSV file
+    writeToCSV(allCombinations, name_of_generated_File)
+
+    
 
